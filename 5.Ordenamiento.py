309143dccb6b7030f3cd7b342c5c7db955984a04
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-01-19 12:22:02 -0500

    Ejercicio ordenamiento

---
+++ 5.Ordenamiento.py
@@ -0,0 +1,8 @@
+# Escriba un programa que reciba como entrada dos números, y los muestre ordenados de menor a mayor:
+
+Numero1 = float(input("ingrese el valor del primer numero \n"))
+Numero2 = float(input("ingrese el valor del segundo numero \n"))
+Orden = max(Numero1, Numero2)
+Orden2 = min(Numero1, Numero2)
+print(f"Menor: {Orden2} Mayor: {Orden}")
+    
